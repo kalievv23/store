4dd53adeb13b6b1851d7849a6d340a04fd990685
kalievv.23 <kaliev2004kg@gmail.com>
2023-07-22 10:24:51 +0600

    structure project

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Header from '../layouts/Header'
+import Main from '../layouts/Main'
+import Aside from '../layouts/Aside'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+      <div>
+        <Aside></Aside>
+        <Main></Main>
+      </div>
+    </div>
   )
+
 }
 
 export default Home
---
+++ src/components/store/Products.js
@@ -0,0 +1,87 @@
+import phone1 from "../../assets/images/phone1.webp";
+import phone2 from "../../assets/images/phone2.webp";
+import phone3 from "../../assets/images/phone3.webp";
+import phone4 from "../../assets/images/phone4.webp";
+import phone5 from "../../assets/images/phone5.webp";
+import phone6 from "../../assets/images/phone6.webp";
+import phone7 from "../../assets/images/phone7.webp";
+import phone8 from "../../assets/images/phone8.webp";
+import phone9 from "../../assets/images/phone9.webp";
+import phone10 from "../../assets/images/phone10.webp";
+import phone11 from "../../assets/images/phone11.webp";
+import phone12 from "../../assets/images/phone12.webp";
+
+export const Products = [
+  {
+    id: 1,
+    title: "Apple iPhone 14 Plus",
+    price: "97990",
+    url: phone1,
+  },
+  {
+    id: 2,
+    title: "Смартфон Xiaomi Poco C55 (6+128) IND",
+    price: "10990",
+    url: phone2,
+  },
+  {
+    id: 3,
+    title: "Смартфон Xiaomi 13 Ultra",
+    price: "25900",
+    url: phone3,
+  },
+  {
+    id: 4,
+    title: "Смартфон Xiaomi Poco",
+    price: "31600",
+    url: phone4,
+  },
+  {
+    id: 5,
+    title: "Смартфон Realme 11 Pro",
+    price: "",
+    url: phone5,
+  },
+  {
+    id: 6,
+    title: "Смартфон Samsung Galaxy A14",
+    price: "12800",
+    url: phone6,
+  },
+  {
+    id: 7,
+    title: "Смартфон Samsung Galaxy M33",
+    price: "15950",
+    url: phone7,
+  },
+  {
+    id: 8,
+    title: "Смартфон Samsung Galaxy S22 Ultra",
+    price: "66500",
+    url: phone8,
+  },
+  {
+    id: 9,
+    title: "Смартфон Apple iPhone 14 Pro Max",
+    price: "",
+    url: phone9,
+  },
+  {
+    id: 10,
+    title: "Смартфон Huawei Nova 9SE",
+    price: "18500",
+    url: phone10,
+  },
+  {
+    id: 11,
+    title: "Смартфон Xiaomi Poco F5 Pro",
+    price: "48900",
+    url: phone11,
+  },
+  {
+    id: 12,
+    title: "Смартфон Samsung Galaxy S23 Plus",
+    price: "66300",
+    url: phone12,
+  },
+];
